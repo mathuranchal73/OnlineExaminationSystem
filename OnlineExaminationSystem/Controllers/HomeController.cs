5326b13bc715050bea4eedacdbe1cd9bb8e7416e
Anchal.Mathur <Anchal.Mathur@BOLD-ANCHALM>
2018-08-03 17:21:31 +0530

    002

--- OnlineExaminationSystem/Controllers/HomeController.cs
+++ OnlineExaminationSystem/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineExaminationSystem.Models;
 
 namespace OnlineExaminationSystem.Controllers
 {
@@ -12,21 +13,34 @@ namespace OnlineExaminationSystem.Controllers
         {
             var _ctx = new OESEntities();
             ViewBag.Tests = _ctx.Tests.Where(x => x.IsActive == 1).Select(x => new { x.Id, x.Name }).ToList();
-                
+
+            SessionModel _model = null;
+            if (Session["SessionModel"] == null)
+                _model = new SessionModel();
+            else
+                _model = (SessionModel)Session["SessionModel"];
+            return View(_model);
+        }
+
+        public ActionResult Instructions(SessionModel model)
+        {
+            if(model!=null)
+            {
+                var _ctx = new OESEntities();
+                var test = _ctx.Tests.Where(x => x.IsActive == 1 && x.Id == model.TestId).FirstOrDefault();
+                if(test!=null)
+                {
+                    ViewBag.TestName = test.Name;
+                    ViewBag.TestDescription = test.Description;
+                    ViewBag.QuestionCount = test.TestXQuestions.Count;
+                    ViewBag.TestDuration = test.DurationInMinute;
+                }
+            }
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Register()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
