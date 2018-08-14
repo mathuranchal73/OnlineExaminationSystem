7fcba27ae1db588840fb20161b2a7b39956cc097
Anchal.Mathur <Anchal.Mathur@BOLD-ANCHALM>
2018-08-14 10:56:34 +0530

    008

---
+++ OnlineExaminationSystem/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineExaminationSystem.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace OnlineExaminationSystem.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AccountController.ConfirmEmail),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+
+        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {     
+            return urlHelper.Action(
+                action: nameof(AccountController.ResetPassword),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+    }
+}
---
+++ OnlineExaminationSystem/Services/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OnlineExaminationSystem.Services
+{
+    public class EmailSender : IEmailSender
+    {
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
