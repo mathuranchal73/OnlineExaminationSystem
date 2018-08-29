ef5a6e1fa77c5d86068ecffffc3592947644088a
Anchal.Mathur <Anchal.Mathur@BOLD-ANCHALM>
2018-08-29 11:17:34 +0530

    API Added

--- API/Controllers/QuestionsController.cs
+++ API/Controllers/QuestionsController.cs
@@ -9,9 +9,11 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using API.Models;
+using System.Web.Http.Cors;
 
 namespace API.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class QuestionsController : ApiController
     {
         private OESEntities db = new OESEntities();
